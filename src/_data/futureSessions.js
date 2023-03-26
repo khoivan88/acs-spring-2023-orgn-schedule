accab3aff0c737c096d6a35769a5f26e9f74fa86
khoivan88 <33493502+khoivan88@users.noreply.github.com>
2023-03-26 12:12:35 -0500

    change to 2023 on web interface

--- src/_data/futureSessions.js
+++ src/_data/futureSessions.js
@@ -1,14 +1,14 @@
 /** Every timestamp is in UTC time */
 const now = new Date();
-// const now = new Date('2022-03-20T20:00:00.000Z');    //! for testing
+// const now = new Date('2023-03-20T20:00:00.000Z');    //! for testing
 // Offset current time by 4 hours
 const nowOffset = new Date(now.getTime() - 4 * 60 * 60 * 1000);
 
-const allSessions = require('./acs_spring2023_orgn.json')
+const allSessions = require("./acs_spring2023_orgn.json");
 
-const futureSessions = allSessions.filter(session => {
-    const sessionDate = new Date(session.starting_datetime);
-    return sessionDate >= nowOffset;
+const futureSessions = allSessions.filter((session) => {
+  const sessionDate = new Date(session.starting_datetime);
+  return sessionDate >= nowOffset;
 });
 // console.log(nowOffset);
 // console.log(futureSessions.length);
